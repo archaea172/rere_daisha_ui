33c9b4c0c9f9747180cd6a4c692ab00bdd8f7c32
aky <akiyugata271@gmail.com>
2025-10-16 17:57:18 +0900

    add subscriber

--- Assets/scripts/Subscriber.cs
+++ Assets/scripts/Subscriber.cs
@@ -1,6 +1,19 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Robotics.ROSTCPConnector;
 
-public class Subscriber
+using BallMsg = RosMessageTypes.RereDaisha.BallPositionArrayMsg;
+public class Subscriber : MonoBehaviour
 {
-    
+
+    void Start()
+    {
+        ROSConnection.instance.Subscribe<BallMsg>("ball_position_yolo", Callback);
+    }
+
+    void Callback(BallMsg rxdata)
+    {
+        Debug.Log(rxdata.balls);
+    }
 }
