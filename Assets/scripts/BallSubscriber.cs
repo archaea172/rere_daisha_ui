3c581f144add8ed4827ba5f37832e23e546eddca
aky <akiyugata271@gmail.com>
2025-10-18 23:09:41 +0900

    update

--- Assets/scripts/BallSubscriber.cs
+++ Assets/scripts/BallSubscriber.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
+using Unity.Robotics.ROSTCPConnector;
+using RosMessageTypes.RereDaisha;
 
 public class BallSubscriber : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ROSConnection.GetOrCreateInstance().Subscribe<BallArrayMsg>("ball_array_topic", Callback);
     }
 
     // Update is called once per frame
-    void Update()
+    void Callback(BallArrayMsg rxdata)
     {
-        
+        Debug.Log($"Received {message.balls.Length} balls.");
     }
 }
