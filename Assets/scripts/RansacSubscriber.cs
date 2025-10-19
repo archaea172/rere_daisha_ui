002716b8e341b7de0a0c799f3a170798fbdd6a33
aky <akiyugata271@gmail.com>
2025-10-19 19:51:28 +0900

    update scripts

--- Assets/scripts/RansacSubscriber.cs
+++ Assets/scripts/RansacSubscriber.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Robotics.ROSTCPConnector;
-using BallPositionArrayMsg = RosMessageTypes.RereDaisha.BallPositionArrayMsg;
+using RosMessageTypes.Sensor;
 using UnityEngine.UI;
 
 public class RansacSubscriber : MonoBehaviour
@@ -13,12 +13,33 @@ public class RansacSubscriber : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ROSConnection.GetOrCreateInstance().Subscribe<PointCloudMsg>("ball_position_ransac", Callback);
     }
-
-    // Update is called once per frame
-    void Update()
+    void Callback(PointCloudMsg rxdata)
     {
-        
+        float grid_weight = 800F;
+        float grid_height = 800F;
+
+        foreach (var point in activePoints)
+        {
+            Destroy(point);
+        }
+        activePoints.Clear();
+
+        foreach (var ball in rxdata.points)
+        {
+            GameObject newBall = Instantiate(ballPrefab, canvasRectTransform);
+
+            Image ballImage = newBall.GetComponent<Image>();
+            ballImage.color = Color.black;
+            RectTransform ballRect = newBall.GetComponent<RectTransform>();
+            float posX = (float)ball.x * grid_weight / 2;
+            float posY = (float)ball.y * grid_height / 2;
+
+            float CenterX = grid_weight / 2;
+            float CenterY = grid_height / 2;
+            ballRect.anchoredPosition = new Vector2(posX + CenterX, posY + CenterY);
+            activePoints.Add(newBall);
+        }
     }
 }
