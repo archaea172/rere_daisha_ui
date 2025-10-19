25b4b2e015c57f74988fe4d60ea171e0c0c638db
aky <akiyugata271@gmail.com>
2025-10-19 17:26:41 +0900

    update

--- Assets/scripts/BallSubscriber.cs
+++ Assets/scripts/BallSubscriber.cs
@@ -6,7 +6,9 @@ using BallPositionArrayMsg = RosMessageTypes.RereDaisha.BallPositionArrayMsg;
 
 public class BallSubscriber : MonoBehaviour
 {
-    public RectTransform Ball;
+    public GameObject ballPrefab;
+    public RectTransform canvasRectTransform;
+    private List<GameObject> activePoints = new List<GameObject>();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,5 +19,25 @@ public class BallSubscriber : MonoBehaviour
     void Callback(BallPositionArrayMsg rxdata)
     {
         Debug.Log($"Received {rxdata.balls.Length} balls.");
+        
+        foreach (var point in activePoints)
+        {
+            Destroy(point);
+        }
+        activePoints.Clear();
+
+        foreach (var ball in rxdata.balls)
+        {
+            GameObject newBall = Instantiate(ballPrefab, canvasRectTransform);
+
+            RectTransform ballRect = newBall.GetComponent<RectTransform>();
+            float posX = (float)ball.position.x;
+            float posY = (float)ball.position.y;
+
+            float CenterX = 502.2F;
+            float CenterY = 500F;
+            ballRect.anchoredPosition = new Vector2(posX + CenterX, posY + CenterY);
+            activePoints.Add(newBall);
+        }
     }
 }
